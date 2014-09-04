fe0583a78e58cbec33554311e96de692368b2eba
Ryan Perez <rrperez@stanford.edu>
2014-09-03 20:53:34 -0700

    rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Schedulr::Application.config.secret_key_base = 'ba5be55688c52e6226d16f3adf889de2ccc1880482f961b9c7e98fbe93f6cb4f8d304a82f746d1f381d7066f0586a2423fe02aae0ea18e935a51b3a9ddd3dac0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Schedulr::Application.config.session_store :cookie_store, key: '_schedulr_session'
